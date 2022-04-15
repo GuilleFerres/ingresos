a20156eeaf6d7cfc9727dac4a55fbef701325b36
GuilleFerres <guillermoferressanchez@gmail.com>
2022-04-15 11:43:25 +0200

    Seccion 7 - Auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+      apiKey: 'AIzaSyDBXKGnL0pJUqdg2wkZ23RDxHgJ9SqsOJ0',
+      authDomain: 'ingreso-egreso-app-414b6.firebaseapp.com',
+      projectId: 'ingreso-egreso-app-414b6',
+      storageBucket: 'ingreso-egreso-app-414b6.appspot.com',
+      messagingSenderId: '642214502643',
+      appId: '1:642214502643:web:aa5aa539c5cb71f993d786',
+      measurementId: 'G-2H0L1G01J2'
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDBXKGnL0pJUqdg2wkZ23RDxHgJ9SqsOJ0',
+    authDomain: 'ingreso-egreso-app-414b6.firebaseapp.com',
+    projectId: 'ingreso-egreso-app-414b6',
+    storageBucket: 'ingreso-egreso-app-414b6.appspot.com',
+    messagingSenderId: '642214502643',
+    appId: '1:642214502643:web:aa5aa539c5cb71f993d786',
+    measurementId: 'G-2H0L1G01J2'
+  }
 };
 
 /*
